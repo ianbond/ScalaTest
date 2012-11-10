c0e83c784da92c9ddce07b8dc9b43ee88b28573e
Ian Bond <ian.r.bond@gmail.com>
2012-11-10 23:06:12 +0000

    new commit

--- src/TestApp.scala
+++ src/TestApp.scala
@@ -1,4 +1,9 @@
 object TestApp extends App {
-	println("Hello, World!")
-	println("some more code")
+	println("Hello, Scala")
+	println(abs(-100))
+	
+	def abs(x: Int) : Int = {
+	  if (x >=0) x else -x
+	}
+	
 }
